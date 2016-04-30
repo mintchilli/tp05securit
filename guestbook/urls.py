dac70d905f8144a424005f9060dae10fefeb4070
mintchilli <poissonpourri@gmail.com>
2016-04-29 21:26:07 -0400

    site cool

--- guestbook/urls.py
+++ guestbook/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/$', login_required(views.post_detail), name='post_detail'),
     url(r'^post/new/$', login_required(views.post_new), name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', login_required(views.post_edit), name='post_edit'),
+    url(r'^post/(?P<pk>\d+)/remove/$', login_required(views.post_remove), name='post_remove'),
+    url(r'^post/(?P<pk>\d+)/comment/$', login_required(views.add_comment_to_post), name='add_comment_to_post'),
+    url(r'^comment/(?P<pk>\d+)/remove/$', login_required(views.comment_remove), name='comment_remove'),
 ]
--- guestbook/views.py
+++ guestbook/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import Group
 from django.utils import timezone
 from django.shortcuts import redirect
-from .models import Post
-from .forms import PostForm
+from django.core.urlresolvers import reverse
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+
 
 
 def post_list(request):
@@ -39,3 +42,28 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
         return render(request, 'guestbook/post_edit.html', {'form': form})
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'guestbook/add_comment_to_post.html', {'form': form})
+
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
